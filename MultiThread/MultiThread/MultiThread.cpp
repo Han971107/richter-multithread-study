c57bb12ec58a773ee5848f9683718e98ec354c30
Han97 <hcw9711@gmail.com>
2026-05-29 02:19:55 +0900

    exercise 02: 매개변수 전달 + WaitForMultipleObjects

--- MultiThread/MultiThread/MultiThread.cpp
+++ MultiThread/MultiThread/MultiThread.cpp
@@ -5,30 +5,46 @@ using namespace std;
 
 DWORD WINAPI ThreadProc(LPVOID lpParameter)
 {
-    cout << "Hello from thread!" << endl;
+    int id = (int)(intptr_t)lpParameter;
+    cout << "[Worker" << " " << id << "] " << "I am thread number " << id << endl;
     return 0;
 }
 
 int main()
 {
-    cout << "[Main] Creating thread..." << endl;
+    constexpr int THREAD_COUNT = 3;
 
-    HANDLE h = ::CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
-    if (h == NULL) {
-        cout << "[Main] CreateThread failed: " << GetLastError() << endl;
-        return 1;
+    cout << "[Main] Creating " << THREAD_COUNT << " threads..." << endl;
+
+    HANDLE handles[THREAD_COUNT] = {};
+    
+    int createdCount = 0;
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+
+        handles[i] = ::CreateThread(NULL, 0, ThreadProc, (LPVOID)(intptr_t)(i + 1), 0, NULL);
+
+        if (handles[i] == NULL) {
+            cout << "[Main] CreateThread failed at index " << i << endl;
+            break;  // 루프 탈출
+        }
+
+        createdCount++;
     }
 
     cout << "[Main] Waiting for thread..." << endl;
 
-    DWORD res = ::WaitForSingleObject(h, INFINITE);
+    DWORD res = ::WaitForMultipleObjects(createdCount, handles, TRUE, INFINITE);
     if (res == WAIT_FAILED) {
         cout << "[Main] Wait failed: " << GetLastError() << endl;
-        ::CloseHandle(h);
+        for (int i = 0; i < createdCount; ++i) {
+            ::CloseHandle(handles[i]);
+        }
         return -1;
     }
 
-    ::CloseHandle(h);
+    for (int i = 0; i < createdCount; ++i) {
+        ::CloseHandle(handles[i]);
+    }
 
     cout << "[Main] Thread finished. Bye!" << endl;
 
