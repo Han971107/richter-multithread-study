7573fb5d1e893610c1364e45739a378b48247fb8
Han97 <hcw9711@gmail.com>
2026-05-29 01:34:16 +0900

    exercise 01: CreateThread 기본 사용 - Hello from thread

---
+++ MultiThread/MultiThread/MultiThread.cpp
@@ -0,0 +1,36 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter)
+{
+    cout << "Hello from thread!" << endl;
+    return 0;
+}
+
+int main()
+{
+    cout << "[Main] Creating thread..." << endl;
+
+    HANDLE h = ::CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
+    if (h == NULL) {
+        cout << "[Main] CreateThread failed: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "[Main] Waiting for thread..." << endl;
+
+    DWORD res = ::WaitForSingleObject(h, INFINITE);
+    if (res == WAIT_FAILED) {
+        cout << "[Main] Wait failed: " << GetLastError() << endl;
+        ::CloseHandle(h);
+        return -1;
+    }
+
+    ::CloseHandle(h);
+
+    cout << "[Main] Thread finished. Bye!" << endl;
+
+    return 0;
+}
