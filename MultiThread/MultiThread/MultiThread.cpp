ba57c149fc8d701411566a1b6c58d653d33dadca
Han97 <hcw9711@gmail.com>
2026-06-06 01:06:27 +0900

    exercise 07: Condition Variable로 Job Queue 구현 (producer-main + worker 4개)

--- MultiThread/MultiThread/MultiThread.cpp
+++ MultiThread/MultiThread/MultiThread.cpp
@@ -3,37 +3,29 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <queue>
 using namespace std;
 
-SRWLOCK g_lock;
-map<int, string> g_assets;   // ID → 자산 이름
-LONG g_writerDone = 0;
-LONG g_successCount = 0;
+CRITICAL_SECTION g_cs;
+CONDITION_VARIABLE g_cv;
+queue<int> g_jobs;
+bool g_shutdown = false;
+LONG g_processedCount = 0;
 
-unsigned __stdcall WriterThreadProc(void* lpParameter)
+unsigned __stdcall WorkerThreadProc(void* lpParameter)
 {
-    for (int i = 0; i < 100; ++i) {
-        int id = rand() % 100 + 1;
-        AcquireSRWLockExclusive(&g_lock);
-        g_assets.insert(std::make_pair(id, std::to_string(id)));
-        ReleaseSRWLockExclusive(&g_lock);
-        Sleep(10);
-    }
-    InterlockedExchange(&g_writerDone, 1);
-
-    return 0;
-}
-
-unsigned __stdcall ReaderThreadProc(void* lpParameter)
-{
-    while (g_writerDone == 0) {
-        int id = rand() % 100 + 1;
-        AcquireSRWLockShared(&g_lock);
-        auto iter = g_assets.find(id);
-        if (iter != g_assets.end()) {
-            InterlockedIncrement(&g_successCount);
+    while (true) {
+        EnterCriticalSection(&g_cs);
+        while (g_jobs.empty() && !g_shutdown) {
+            SleepConditionVariableCS(&g_cv, &g_cs, INFINITE);
         }
-        ReleaseSRWLockShared(&g_lock);
+        if (g_jobs.empty()) {
+            LeaveCriticalSection(&g_cs);
+            break;
+        }
+        g_jobs.pop();
+        LeaveCriticalSection(&g_cs);
+        InterlockedIncrement(&g_processedCount);
     }
 
     return 0;
@@ -41,25 +33,18 @@ unsigned __stdcall ReaderThreadProc(void* lpParameter)
 
 int main()
 {
-    srand((unsigned)time(NULL));        
-    InitializeSRWLock(&g_lock);
+    srand((unsigned)time(NULL));
+    InitializeCriticalSection(&g_cs);
+    InitializeConditionVariable(&g_cv);
 
-    // Writer
-    HANDLE handle = (HANDLE)_beginthreadex(NULL, 0, WriterThreadProc, (void*)(intptr_t)(1), 0, NULL);
-    if (handle == 0) {
-        cout << "Failed creating thread..." << endl;
-        cout << GetLastError() << endl;
-        return -1;
-    }
-
-    constexpr int readerThreadCount = 9;
+    constexpr int readerThreadCount = 4;
 
     HANDLE readHandles[readerThreadCount] = {};
 
     int createdThread = 0;
     for (int i = 0; i < readerThreadCount; ++i) {
 
-        readHandles[i] = (HANDLE)_beginthreadex(NULL, 0, ReaderThreadProc, (void*)(intptr_t)(i + 1), 0, NULL);
+        readHandles[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThreadProc, (void*)(intptr_t)(i + 1), 0, NULL);
 
         if (readHandles[i] == 0) {
             cout << "Failed creating thread..." << endl;
@@ -69,6 +54,16 @@ int main()
 
         ++createdThread;
     }
+
+    for (int i = 0; i < 1000; ++i) {
+        EnterCriticalSection(&g_cs);
+        g_jobs.push(i + 1);
+        LeaveCriticalSection(&g_cs);
+        WakeConditionVariable(&g_cv);
+    }
+
+    g_shutdown = true;
+    WakeAllConditionVariable(&g_cv);
 
     DWORD res = ::WaitForMultipleObjects(createdThread, readHandles, TRUE, INFINITE);
     if (res == WAIT_FAILED) {
@@ -80,22 +75,12 @@ int main()
 
         return -1;
     }
-    
-    res = ::WaitForSingleObject(handle, INFINITE);
-    if (res == WAIT_FAILED) {
-        cout << "[Main] Wait failed: " << GetLastError() << endl;
-        ::CloseHandle(handle);
-        return -1;
-    }
 
     for (int i = 0; i < createdThread; ++i) {
         ::CloseHandle(readHandles[i]);
     }
 
-    ::CloseHandle(handle);
-
-    cout << "총 데이터 수: " << g_assets.size() << endl;
-    cout << "성공 횟수: " << g_successCount << endl;
+    cout << g_processedCount << endl;
 
     return 0;
 }
